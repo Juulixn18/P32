725d40cda9a05c183574fb6f3180491c63b3103f
Juulixn18 <julianalexander563@gmail.com>
2025-03-07 11:44:17 +0100

    Practica 32

---
+++ P32/src/p32/Clase1.java
@@ -0,0 +1,9 @@
+package p32;
+
+public class Clase1 {
+    public static void main(String[] args) {
+        System.out.println("Hola, soy la clase Main");
+        Clase2 saludo = new Clase2();
+        saludo.saludar();
+    }
+}
---
+++ P32/src/p32/Clase2.java
@@ -0,0 +1,7 @@
+package p32;
+
+public class Clase2 {
+    public void saludar() {
+        System.out.println("Hola, soy la clase auxiliar");
+    }
+}
